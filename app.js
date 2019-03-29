510bd2cfa171bf802eab9d80cfbb56109b02727f
unknown <mjawwadiqbal22@gmail.com>
2019-03-28 22:34:36 -0700

    Readline Module

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
-//1
-console.log('Hello')
-//2
-const tutorial =require('./tutorial');
-console.log(tutorial.sum(1,1));
-console.log(tutorial.Pi);
+// //1
+// console.log('Hello')
+// //2
+// const tutorial =require('./tutorial');
+// console.log(tutorial.sum(1,1));
+// console.log(tutorial.Pi);
 
-//3
+// //3
 
-const EventEmitter=require('events');
-const eventEmitter=new EventEmitter();
+// const EventEmitter=require('events');
+// const eventEmitter=new EventEmitter();
 
-//event jab chala ga jab tutorial event run hoga
-eventEmitter.on('tutorial',()=>{
-    console.log('yeh tutorial chala');
+// //event jab chala ga jab tutorial event run hoga
+// eventEmitter.on('tutorial',()=>{
+//     console.log('yeh tutorial chala');
+// });
+// eventEmitter.on('tutorialtwo',(num1,num2)=>{
+//     console.log(num1+num2);
+// });
+
+// eventEmitter.emit('tutorial');
+// eventEmitter.emit('tutorialtwo',2,3);
+
+// class Person extends EventEmitter{
+//     constructor(name){
+//         super();
+//         this._name=name;
+//     }
+//     get name(){
+//         return this._name;
+//     }
+// }
+// let pedro=new Person('Pedro');
+// let hena=new Person('hena');
+// pedro.on('name',()=>{
+//     console.log('my name is '+ pedro.name);
+// })
+// hena.on('name',()=>{
+//     console.log(hena.name)
+// })
+
+// pedro.emit('name');
+// hena.emit('name');
+
+//4
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
-eventEmitter.on('tutorialtwo',(num1,num2)=>{
-    console.log(num1+num2);
-});
 
-eventEmitter.emit('tutorial');
-eventEmitter.emit('tutorialtwo',2,3);
+let num1=Math.floor((Math.random()*10)+1);
+let num2=Math.floor((Math.random()*10)+1);
+let answer=num1+num2;
 
-class Person extends EventEmitter{
-    constructor(name){
-        super();
-        this._name=name;
+rl.question(`what is ${num1}+${num2}?\n`,(userInput)=>{
+    console.log(userInput);
+    if(userInput.trim()==answer){
+        rl.close();
     }
-    get name(){
-        return this._name;
-    }
+    else {
+        rl.setPrompt('Incorrect response please try again \n');
+    rl.prompt();
+    
 }
-let pedro=new Person('Pedro');
-let hena=new Person('hena');
-pedro.on('name',()=>{
-    console.log('my name is '+ pedro.name);
-})
-hena.on('name',()=>{
-    console.log(hena.name)
-})
 
-pedro.emit('name');
-hena.emit('name');
+});
+
+rl.on('close',()=>{
+    console.log('Correct!!!');
+})
