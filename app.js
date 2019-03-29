55ea4020e9e631be826e589e17af61d4b9e5ab7c
unknown <mjawwadiqbal22@gmail.com>
2019-03-28 21:50:55 -0700

    --Events Modules and Event Emitter Class

--- app.js
+++ app.js
@@ -4,3 +4,40 @@ console.log('Hello')
 const tutorial =require('./tutorial');
 console.log(tutorial.sum(1,1));
 console.log(tutorial.Pi);
+
+//3
+
+const EventEmitter=require('events');
+const eventEmitter=new EventEmitter();
+
+//event jab chala ga jab tutorial event run hoga
+eventEmitter.on('tutorial',()=>{
+    console.log('yeh tutorial chala');
+});
+eventEmitter.on('tutorialtwo',(num1,num2)=>{
+    console.log(num1+num2);
+});
+
+eventEmitter.emit('tutorial');
+eventEmitter.emit('tutorialtwo',2,3);
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name=name;
+    }
+    get name(){
+        return this._name;
+    }
+}
+let pedro=new Person('Pedro');
+let hena=new Person('hena');
+pedro.on('name',()=>{
+    console.log('my name is '+ pedro.name);
+})
+hena.on('name',()=>{
+    console.log(hena.name)
+})
+
+pedro.emit('name');
+hena.emit('name');
