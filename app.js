0d4930f73e9047efc841090cb663eb1e165692b8
unknown <mjawwadiqbal22@gmail.com>
2019-03-29 15:19:25 -0700

    route and query parameter

--- app.js
+++ app.js
@@ -160,5 +160,13 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+app.get('/example',(req,res)=>{
+    res.send('hitting example route');
+});
+app.get('/example/:name/:age',(req,res)=>{
+console.log(req.params);
+console.log(req.query);
+res.send('example with param'+req.params.name+req.params.age)
+});
 
 app.listen('3000');
