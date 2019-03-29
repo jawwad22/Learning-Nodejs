6e2908923356ac7a12f31ca4107623397fa939a6
unknown <mjawwadiqbal22@gmail.com>
2019-03-28 21:40:05 -0700

    Working with Modules

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+//1
 console.log('Hello')
+//2
+const tutorial =require('./tutorial');
+console.log(tutorial.sum(1,1));
+console.log(tutorial.Pi);
---
+++ tutorial.js
@@ -0,0 +1,14 @@
+
+const sum=(num1,num2)=>num1+num2;
+const Pi=3.14;
+
+class SomeMathObject{
+    constructor(){
+        console.log('object created')
+    }
+}
+module.exports={
+    sum:sum,
+    Pi:Pi,
+    SomeMathObject:SomeMathObject
+}
